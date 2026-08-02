53e0c3f59a7bde0c2bd3121bd89a0ae32c0d24b6
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-02 17:54:46 +0530

    add

--- 01_Arrays_and_Hashing/ContainsDuplicate.java
+++ 01_Arrays_and_Hashing/ContainsDuplicate.java
@@ -3,21 +3,24 @@ package Arrays_and_Hashing;
 import java.util.*;
 
 /**
- * Time Complexity:  
- * Space Complexity:
+ * Time Complexity:  O(n)
+ * Space Complexity: O(n)
  */
 public class ContainsDuplicate {
 
     public static boolean containsDuplicate(int[] nums) {
-        return true;
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            if(set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
     }
 
     public static void main(String[] args) { run(); }
 
     public static void run() {
-        System.out.println("\n--- Contains Duplicate ---");
-        System.out.println("containsDuplicate([1,2,3,1])    => " + containsDuplicate(new int[]{1, 2, 3, 1}));    // true
-        System.out.println("containsDuplicate([1,2,3,4])    => " + containsDuplicate(new int[]{1, 2, 3, 4}));    // false
-        System.out.println("containsDuplicate([1,1,1,3,3,4,3,2,4,2]) => " + containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2})); // true
+        int[] a = {1,1,1,3,3,4,3,2,4,2};
+        System.out.println(containsDuplicate(a));
     }
 }
