3689679270f6358e082d15b7cfb51257dfbe0e11
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-08 16:13:53 +0530

    add prob11

---
+++ 02_Two_Pointers/MaxArea.java
@@ -0,0 +1,30 @@
+package Arrays_and_Hashing;
+
+/**
+ * Time Complexity: O(n)
+ * Space Complexity: O(1)
+ */
+public class MaxArea {
+
+    public static int maxArea(int[] h) {
+        int n = h.length;
+        int maxWater = Integer.MIN_VALUE;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int water = Math.min(h[l], h[r]) * (r - l);
+            maxWater = Math.max(maxWater, water);
+            if(h[l] < h[r]) l++;
+            else r--;
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        int[] h = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(maxArea(h));
+    }
+}
