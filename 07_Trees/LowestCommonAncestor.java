fc3926c578b5f3ab9c4afafa94c1d52cc01ab00d
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-08 10:00:00 +0530

    fix main method

--- 07_Trees/LowestCommonAncestor.java
+++ 07_Trees/LowestCommonAncestor.java
@@ -24,24 +24,21 @@ public class LowestCommonAncestor {
         run();
     }
 
-    private static List<Integer> levelOrder(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        for (q.offer(root); !q.isEmpty(); ) {
-            TreeNode n = q.poll();
-            res.add(n.val);
-            if (n.left != null) q.offer(n.left);
-            if (n.right != null) q.offer(n.right);
-        }
-        return res;
-    }
-
     public static void run() {
-        // Input: [4,2,7,1,3,6,9]
-        TreeNode root = new TreeNode(4,
-                new TreeNode(2, new TreeNode(1), new TreeNode(3)),
-                new TreeNode(7, new TreeNode(6), new TreeNode(9)));
+        // Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+        // Expected Output: 6
+        TreeNode root = new TreeNode(6,
+                new TreeNode(2,
+                        new TreeNode(0),
+                        new TreeNode(4, new TreeNode(3), new TreeNode(5))),
+                new TreeNode(8,
+                        new TreeNode(7),
+                        new TreeNode(9)));
 
-        System.out.println("Output: " + levelOrder(lowestCommonAncestor(root, p, q)));
+        TreeNode p = new TreeNode(2);
+        TreeNode q = new TreeNode(8);
+
+        TreeNode lca = lowestCommonAncestor(root, p, q);
+        System.out.println("Output: " + (lca != null ? lca.val : "null"));
     }
 }
