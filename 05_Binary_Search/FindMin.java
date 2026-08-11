4f63af6d5f4f44baefeeb6595ec4a349d02ee2d2
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-11 12:20:30 +0530

    add prob17

---
+++ 05_Binary_Search/FindMin.java
@@ -0,0 +1,39 @@
+package Binary_Search;
+
+public class FindMin {
+    public static int findMin(int[] nums) {
+        int n = nums.length;
+        if(nums[0] < nums[n-1]) return nums[0];
+
+        int start = 0;
+        int end = n - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int next = (mid + 1) % n;   // if mid is last
+            int prev = (mid - 1 + n) % n;   // if mid is first
+
+            // minimum element would be smaller than both neighbours
+            if (nums[mid] <= nums[next] && nums[mid] <= nums[prev]) {
+                return nums[mid];
+            }
+
+            // this half is sorted so eliminate it since min. element will lie in the unsorted half
+            else if (nums[0] <= nums[mid]) {
+                start = mid + 1;
+            }
+
+            else
+                end = mid - 1;
+        }
+        return nums[0];
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        int[] a = {3,4,5,1,2};
+        System.out.println(findMin(a));
+    }
+}
