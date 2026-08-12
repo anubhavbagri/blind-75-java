2c09d52ffac9def7b88951547aaec17ceafb817a
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-12 13:08:42 +0530

    add prob20

---
+++ 06_Linked_List/MergeTwoLists.java
@@ -0,0 +1,50 @@
+package Linked_List;
+
+import Linked_List.ListNode;
+
+/**
+ * Time Complexity: O(n + m)
+ * Space Complexity: O(1)
+ */
+
+public class MergeTwoLists {
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if(list1 != null){
+            curr.next = list1;
+        }
+
+        if(list2 != null){
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        ListNode list1 = new ListNode(1, new ListNode(3, new ListNode(5)));
+        ListNode list2 = new ListNode(2, new ListNode(4, new ListNode(6)));
+        ListNode result = mergeTwoLists(list1, list2);
+        while (result != null) {
+            System.out.print(result.val + (result.next != null ? " -> " : "\n"));
+            result = result.next;
+        }
+    }
+}
