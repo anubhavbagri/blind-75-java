83ed1f22829c44fd309bcb6ed9528d6d2cc82cdf
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-15 23:01:42 +0530

    add prob24

---
+++ 15_Greedy/MaxSubArray.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.util.*;
+
+/**
+ * Time Complexity:  O(n)
+ * Space Complexity: O(1)
+ */
+public class MaxSubArray {
+
+    public static int maxSubArray(int[] nums) {
+        int currMax = nums[0];
+        int overallMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currMax = Math.max(nums[i], currMax + nums[i]);
+            overallMax = Math.max(currMax, overallMax);
+        }
+        return overallMax;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        int[] a = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Output = " + maxSubArray(a));
+    }
+}
