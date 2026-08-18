b5ca51c6bfec4acc6cb912f65d7752c63cf633c8
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-18 20:56:45 +0530

    missing corner case

--- 16_Intervals/CanAttendMeetings.java
+++ 16_Intervals/CanAttendMeetings.java
@@ -10,6 +10,7 @@ import java.util.*;
 public class CanAttendMeetings {
 
     public static boolean canAttendMeetings(int[][] intervals) {
+        if(intervals.length == 0)   return true;
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
         for (int i = 1; i < intervals.length; i++) {
             if (intervals[i-1][1] > intervals[i][0])
