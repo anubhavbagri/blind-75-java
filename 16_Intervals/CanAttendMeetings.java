66c9ab7c6fec58ec31154873f6189deb925438fc
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-18 20:55:26 +0530

    add prob29

---
+++ 16_Intervals/CanAttendMeetings.java
@@ -0,0 +1,29 @@
+package Intervals;
+
+import java.util.*;
+
+/**
+ * sorting the 2D array based on end
+ * Time Complexity:  O(n log n)
+ * Space Complexity: O(1)
+ */
+public class CanAttendMeetings {
+
+    public static boolean canAttendMeetings(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i-1][1] > intervals[i][0])
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        int[][] intervals = {{2, 4}, {9, 12}, {6, 10}};
+        System.out.println("Output = " + canAttendMeetings(intervals));
+    }
+}
