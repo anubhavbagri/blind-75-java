ea13a2ace60944010512cab427b11b44177128d5
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-21 10:58:03 +0530

    add prob34

---
+++ 18_Bit_Manipulation/HammingWeight.java
@@ -0,0 +1,26 @@
+package Bit_Manipulation;
+
+/**
+ * Time Complexity: O(log n)
+ * Space Complexity: O(1)
+ */
+public class HammingWeight {
+
+    public static int hammingWeight(int n) {
+        int cnt = 0;
+        while(n > 0){
+            n = (n & (n-1));
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+        int n = 11;
+        System.out.println("Count of set bits = " + hammingWeight(n));
+    }
+}
