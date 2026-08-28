f0a37c7de0011835edf897694765367af802c718
anubhavbagri <anubhavbagri01@gmail.com>
2026-08-29 02:53:41 +0530

    optimized approach

--- 02_Two_Pointers/TrappingRainwater.java
+++ 02_Two_Pointers/TrappingRainwater.java
@@ -1,25 +1,30 @@
 package Two_Pointers;
 
 /**
- * Time Complexity: O(n * n)
- * Space Complexity: O(1)
+ * Time Complexity: O(3n)
+ * Space Complexity: O(2n)
  */
 public class TrappingRainwater {
 
     public static int trap(int[] height) {
         int n = height.length;
         int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int leftMax = Integer.MIN_VALUE;
-            int rightMax = Integer.MIN_VALUE;
 
-            for (int j = 0; j <= i; j++) {
-                leftMax = Math.max(leftMax, height[j]);
-            }
-            for (int k = i; k < n; k++) {
-                rightMax = Math.max(rightMax, height[k]);
-            }
-            ans += Math.min(leftMax, rightMax) - height[i];
+        int leftMax[] = new int[n];
+        int rightMax[] = new int[n];
+
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            ans += Math.min(leftMax[i], rightMax[i]) - height[i];
         }
         return ans;
     }
