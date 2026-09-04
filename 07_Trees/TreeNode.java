5d88a9383aa8ac4518f3d740a343a5af69461a7a
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-04 12:52:56 +0530

    add approach

--- 07_Trees/TreeNode.java
+++ 07_Trees/TreeNode.java
@@ -1,5 +1,12 @@
 package Trees;
-
+/*
+Mental template to develop for trees:
+1. What should happen at the current node?
+2. Can the same problem be applied to the left subtree?
+3. Can the same problem be applied to the right subtree?
+4. What is my base case?
+5. What do I need to return to the caller?
+ */
 /**
  * Definition for a binary tree node.
  */
