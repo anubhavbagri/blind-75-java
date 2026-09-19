366a5467cb9ea6bf68492344106fc21ab0e89bd7
anubhavbagri <anubhavbagri01@gmail.com>
2026-09-19 17:48:28 +0530

    add skeleton

---
+++ 11_Graphs/NumIslands.java
@@ -0,0 +1,24 @@
+package Graphs;
+
+import java.util.*;
+
+/**
+ * Time Complexity:
+ * Space Complexity:
+ */
+
+public class NumIslands {
+
+    public static int numIslands(char[][] grid) {
+
+    }
+
+    public static void main(String[] args) {
+        run();
+    }
+
+    public static void run() {
+
+        System.out.println(numIslands(grid));
+    }
+}
